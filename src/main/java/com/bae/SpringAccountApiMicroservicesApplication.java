348b8de4329d34c7fbd6d108c3db0a452439c9dd
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-05 17:37:53 +0100

    Renamed project

--- src/main/java/com/bae/SpringAccountApiMicroservicesApplication.java
+++ src/main/java/com/bae/SpringAccountApiMicroservicesApplication.java
@@ -2,13 +2,12 @@ package com.bae;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringAccountApiMicroservicesApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext ac = SpringApplication.run(SpringAccountApiMicroservicesApplication.class, args);
+		SpringApplication.run(SpringAccountApiMicroservicesApplication.class, args);
 	}
 
 }
