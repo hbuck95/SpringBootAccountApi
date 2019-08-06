c7e5675b7dee8b6d6c551552546a26f679ba867c
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-06 12:47:03 +0100

    Added tests for CustomerService

--- src/main/java/com/bae/SpringAccountApiApplication.java
+++ src/main/java/com/bae/SpringAccountApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringAccountApiMicroservicesApplication {
+public class SpringAccountApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringAccountApiMicroservicesApplication.class, args);
+		SpringApplication.run(SpringAccountApiApplication.class, args);
 	}
 
 }
